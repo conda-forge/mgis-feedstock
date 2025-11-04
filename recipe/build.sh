231c10412bcda6dbbf64af29a4ac1d7cbe83e426
krande <kristoffer_andersen@outlook.com>
2025-11-04 09:20:25 +0100

    MNT: Update build script to create build directory and change to it before cmake

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-cmake .. \
+mkdir build
+cd build
+
+cmake ${SRC_DIR} \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
     -Denable-c-bindings=OFF \
